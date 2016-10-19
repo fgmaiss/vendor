c56bedf331ed3054c1396fc046ed68e3000c51d6
Suporte Tecnico FGMAISS <admin@fgmaiss.com.br>
2016-10-19 15:04:38 -0200

    Atualizando commit

---
+++ FGMGeneric/APCnpjAndre.php
@@ -0,0 +1,67 @@
+<?php
+namespace FGMGeneric;
+
+/**
+ * Class APCnpjAndre
+ * @package FGMGeneric
+ */
+class APCnpjAndre
+{
+    /**
+     * @var
+     */
+    private $id;
+    /**
+     * @var
+     */
+    private $nome;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * Retorna Máscara do CNPJ
+     * @param $var string
+     * @return string
+     */
+    public static function Maskcnpj($var) {
+        // ---var = string a ser tratada
+
+        $var = \FGMGeneric\APFilter::Number($var);
+        if (strlen(trim($var)) == 11) {
+            $var = substr($var, 0, 3) . '.' . substr($var, 3, 3) . '.' . substr($var, 6, 3) . '-' . substr($var, 9, 2);
+        } elseif (strlen(trim($var)) == 14) {
+            $var = substr($var, 0, 2) . '.' . substr($var, 2, 3) . '.' . substr($var, 5, 3) . '/' . substr($var, 8, 4) . '-' . substr($var, 12, 2);
+        }
+        return ($var);
+    }
+}
